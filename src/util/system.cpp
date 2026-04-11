db8a6d3ab4c38abefc9dba6883310d5104851c7b
TaobotX11 <eudor.olshop@gmail.com>
2026-04-11 16:57:20 +0700

    Strip any trailing / in -datadir and -blocksdir paths #20080

--- src/util/system.cpp
+++ src/util/system.cpp
@@ -29,6 +29,7 @@
 #endif // __linux__
 
 #include <algorithm>
+#include <cassert>
 #include <fcntl.h>
 #include <sched.h>
 #include <sys/resource.h>
@@ -643,10 +644,9 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Nusacoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Nusacoin
-    // Mac: ~/Library/Application Support/Nusacoin
-    // Unix: ~/.nusacoin
+    // Windows: C:\Users\Username\AppData\Roaming\Nusacoin
+    // macOS: ~/Library/Application Support/Nusacoin
+    // Unix-like: ~/.nusacoin
 #ifdef WIN32
     // Windows
     return GetSpecialFolderPath(CSIDL_APPDATA) / "Nusacoin";
@@ -658,14 +658,28 @@ fs::path GetDefaultDataDir()
     else
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
-    // Mac
+    // macOS
     return pathRet / "Library/Application Support/Nusacoin";
 #else
-    // Unix
+    // Unix-like
     return pathRet / ".nusacoin";
 #endif
 #endif
 }
+
+namespace {
+fs::path StripRedundantLastElementsOfPath(const fs::path& path)
+{
+    auto result = path;
+    while (result.filename().string() == ".") {
+        result = result.parent_path();
+    }
+
+    assert(fs::equivalent(result, path));
+    return result;
+}
+} // namespace
+
 
 static fs::path g_blocks_path_cache_net_specific;
 static fs::path pathCached;
@@ -694,6 +708,7 @@ const fs::path &GetBlocksDir()
     path /= BaseParams().DataDir();
     path /= "blocks";
     fs::create_directories(path);
+    path = StripRedundantLastElementsOfPath(path);
     return path;
 }
 
@@ -724,6 +739,7 @@ const fs::path &GetDataDir(bool fNetSpecific)
         fs::create_directories(path / "wallets");
     }
 
+    path = StripRedundantLastElementsOfPath(path);
     return path;
 }
 
