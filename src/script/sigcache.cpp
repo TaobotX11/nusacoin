29ac41765351dc6e3866ccaf0f3148d9f10de7df
TaobotX11 <eudor.olshop@gmail.com>
2026-04-22 22:42:38 +0700

    Declare de facto const reference variables/member functions as const #20584

--- src/script/sigcache.cpp
+++ src/script/sigcache.cpp
@@ -55,7 +55,7 @@ public:
         return setValid.contains(entry, erase);
     }
 
-    void Set(uint256& entry)
+    void Set(const uint256& entry)
     {
         boost::unique_lock<boost::shared_mutex> lock(cs_sigcache);
         setValid.insert(entry);
