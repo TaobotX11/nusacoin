c87ac9aeb88bbb31dbeafcc82bd95f0d18f91873
TaobotX11 <eudor.olshop@gmail.com>
2026-04-26 00:23:27 +0700

    Elide long strings in their middle in the Peers tab #276

--- src/qt/locale/nusacoin_id.ts
+++ src/qt/locale/nusacoin_id.ts
@@ -1780,8 +1780,8 @@ Signing is only possible with addresses of the type &apos;legacy&apos;.</source>
         <translation>Ping</translation>
     </message>
     <message>
-        <source>Direction</source>
-        <translation>Arah</translation>
+        <source>Type</source>
+        <translation>Jenis</translation>
     </message>
     <message>
         <source>Inbound</source>
@@ -2049,8 +2049,8 @@ Signing is only possible with addresses of the type &apos;legacy&apos;.</source>
         <translation>Whitelist</translation>
     </message>
     <message>
-        <source>Connection Type</source>
-        <translation>Jenis Koneksi</translation>
+        <source>Direction/Type</source>
+        <translation>Arah/Jenis</translation>
     </message>
     <message>
         <source>The type of peer connection:&lt;ul&gt;&lt;li&gt;Inbound: initiated by peer&lt;/li&gt;&lt;li&gt;Outbound Full Relay: default&lt;/li&gt;&lt;li&gt;Outbound Block Relay: does not relay transactions or addresses&lt;/li&gt;&lt;li&gt;Outbound Manual: added using RPC %1 or %2/%3 configuration options&lt;/li&gt;&lt;li&gt;Outbound Feeler: short-lived, for testing addresses&lt;/li&gt;&lt;li&gt;Outbound Address Fetch: short-lived, for soliciting addresses&lt;/li&gt;&lt;/ul&gt;</source>
