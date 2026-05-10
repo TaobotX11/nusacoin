3d97880e4881929fa1c6efb0735386706f612a1e
TaobotX11 <eudor.olshop@gmail.com>
2026-05-10 22:00:16 +0700

    fix translation

--- src/qt/locale/nusacoin_id.ts
+++ src/qt/locale/nusacoin_id.ts
@@ -191,8 +191,12 @@ Signing is only possible with addresses of the type &apos;legacy&apos;.</source>
         <translation>Tampilkan passphrase</translation>
     </message>
     <message>
-        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR NusacoinS&lt;/b&gt;!</source>
-        <translation>Peringatan: Jika Anda mengenkripsi dompet Anda dan lupa kata sandi Anda, Anda akan &lt;b&gt;KEHILANGAN SEMUA NUSACOIN ANDA&lt;/b&gt;!</translation>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will </source>
+        <translation>Peringatan: Jika Anda mengenkripsi dompet dan lupa kata sandi Anda, Anda akan </translation>
+    </message>
+    <message>
+        <source>LOSE ALL OF YOUR NUX!</source>
+        <translation>KEHILANGAN SEMUA NUX ANDA!</translation>
     </message>
     <message>
         <source>Are you sure you wish to encrypt your wallet?</source>
